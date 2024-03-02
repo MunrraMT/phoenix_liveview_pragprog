4fa3882d3f5aba9a6fff56d94c9df37b728a2fe4
MunrraMT <dev.armsantos@gmail.com>
2024-03-02 01:02:11 -0400

    Build the Demographic Show Function Component - Using Dynamic Function Components

--- cap6/pento/lib/pento_web/live/demographic_live/show.ex
+++ cap6/pento/lib/pento_web/live/demographic_live/show.ex
@@ -6,17 +6,30 @@ defmodule PentoWeb.DemographicLive.Show do
   import Phoenix.HTML.Form
 
   alias Pento.Survey.Demographic
+  alias PentoWeb.CoreComponents
 
   attr :demographic, Demographic, required: true
 
   def details(assigns) do
     ~H"""
     <div>
-      <h2 class="font-medium text-2xl">Demographic <%= raw("&#x2713;") %></h2>
-      <ul>
+      <h2 class="font-medium text-2xl">
+        Demographic <%= raw("&#x2713;") %>
+      </h2>
+
+      <CoreComponents.table rows={[@demographic]} id={to_string(@demographic.id)}>
+        <:col :let={demographic} label="Gender">
+          <%= demographic.gender %>
+        </:col>
+        <:col :let={demographic} label="Year of birth">
+          <%= demographic.year_of_birth %>
+        </:col>
+      </CoreComponents.table>
+
+      <%!-- <ul>
         <li>Gender: <%= @demographic.gender %></li>
         <li>Year of birth: <%= @demographic.year_of_birth %></li>
-      </ul>
+      </ul> --%>
     </div>
     """
   end
