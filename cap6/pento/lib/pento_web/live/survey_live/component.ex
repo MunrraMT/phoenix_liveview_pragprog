ff5b73d1f10b005a7b617a293936fbb8147fc8ad
MunrraMT <dev.armsantos@gmail.com>
2024-03-02 00:45:24 -0400

    Build the Demographic Show Function Component - Render the Demographic Show Function Component

--- cap6/pento/lib/pento_web/live/survey_live/component.ex
+++ cap6/pento/lib/pento_web/live/survey_live/component.ex
@@ -6,17 +6,17 @@ defmodule PentoWeb.SurveyLive.Component do
 
   def hero(assigns) do
     ~H"""
-    <h1 class="font-heavy text-3xl">
+    <h1 class="text-4xl">
       <%= @content %>
     </h1>
     <h2>
       <%= render_slot(@inner_block) %>
     </h2>
-    <pre>
-    <%= inspect(assigns, pretty: true)  %>
+    <!--<pre>
+    <%= inspect(assigns, pretty: true) %>
     <% %{ inner_block: [%{inner_block: block_fn}]} = assigns %>
     <%= inspect(block_fn.(assigns.__changed__, assigns), pretty: true) %>
-    </pre>
+    </pre>-->
     """
   end
 end
