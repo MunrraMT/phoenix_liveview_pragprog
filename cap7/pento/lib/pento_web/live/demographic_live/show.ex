c22360dbe39159ea7cacc86cfc9f359aabbf1d6a
MunrraMT <dev.armsantos@gmail.com>
2024-03-02 01:07:11 -0400

    cap7 init

---
+++ cap7/pento/lib/pento_web/live/demographic_live/show.ex
@@ -0,0 +1,36 @@
+defmodule PentoWeb.DemographicLive.Show do
+  use Phoenix.Component
+  use PhoenixHTMLHelpers
+
+  import Phoenix.HTML
+  import Phoenix.HTML.Form
+
+  alias Pento.Survey.Demographic
+  alias PentoWeb.CoreComponents
+
+  attr :demographic, Demographic, required: true
+
+  def details(assigns) do
+    ~H"""
+    <div>
+      <h2 class="font-medium text-2xl">
+        Demographic <%= raw("&#x2713;") %>
+      </h2>
+
+      <CoreComponents.table rows={[@demographic]} id={to_string(@demographic.id)}>
+        <:col :let={demographic} label="Gender">
+          <%= demographic.gender %>
+        </:col>
+        <:col :let={demographic} label="Year of birth">
+          <%= demographic.year_of_birth %>
+        </:col>
+      </CoreComponents.table>
+
+      <%!-- <ul>
+        <li>Gender: <%= @demographic.gender %></li>
+        <li>Year of birth: <%= @demographic.year_of_birth %></li>
+      </ul> --%>
+    </div>
+    """
+  end
+end
