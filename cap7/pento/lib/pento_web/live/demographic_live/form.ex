9eec30a88e012ddd4073ee3e8d3de2c2c7dde906
MunrraMT <dev.armsantos@gmail.com>
2024-03-02 21:46:17 -0400

    Build the Live Demographic Form Component - Define the Live Component

---
+++ cap7/pento/lib/pento_web/live/demographic_live/form.ex
@@ -0,0 +1,28 @@
+defmodule PentoWeb.DemographicLive.Form do
+  use PentoWeb, :live_component
+
+  alias Pento.Survey
+  alias Pento.Survey.Demographic
+  alias Pento.Accounts.User
+  alias Phoenix.Socket
+
+  def update(assigns, socket) do
+    {:ok,
+     socket
+     |> assign(assigns)
+     |> assign_demographic()
+     |> clear_form()}
+  end
+
+  defp assign_demographic(%Socket{assigns: %{current_user: %User{} = user}} = socket) do
+    assign(socket, :demographic, %Demographic{user_id: user.id})
+  end
+
+  defp assign_form(%Socket{} = socket, changeset) do
+    assign(socket, :form, to_form(changeset))
+  end
+
+  defp clear_form(%Socket{assigns: %{demographic: demographic}} = socket) do
+    assign_form(socket, Survey.change_demographic(demographic))
+  end
+end
