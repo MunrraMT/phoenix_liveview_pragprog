734750e32f664e3e84d24ced981a8c5f92305bdb
MunrraMT <dev.armsantos@gmail.com>
2024-03-06 01:02:46 -0400

    Show the Rating Form - Update the Rating Index

--- cap7/pento/lib/pento_web/live/survey_live.ex
+++ cap7/pento/lib/pento_web/live/survey_live.ex
@@ -53,6 +53,22 @@ defmodule PentoWeb.SurveyLive do
     |> assign(:demographic, demographic)
   end
 
+  def handle_info({:created_rating, updated_product, product_index}, socket) do
+    {:noreply,
+     socket
+     |> handle_rating_created(updated_product, product_index)}
+  end
+
+  def handle_rating_created(
+        %{assigns: %{products: products}} = socket,
+        updated_product,
+        product_index
+      ) do
+    socket
+    |> put_flash(:info, "Rating submitted successfully")
+    |> assign(:products, List.replace_at(products, product_index, updated_product))
+  end
+
   defp list_products(user) do
     Catalog.list_products_with_user_rating(user)
   end
