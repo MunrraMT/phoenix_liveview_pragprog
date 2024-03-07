27986890697117b3c07bd9dd0209b3136be83b9e
MunrraMT <dev.armsantos@gmail.com>
2024-03-07 00:21:33 -0400

    Initialize the Admin.SurveyResultsLive Component State - Add Survey Results to the Component Socket

--- cap8/pento/lib/pento_web/live/admin/survey_results_live.ex
+++ cap8/pento/lib/pento_web/live/admin/survey_results_live.ex
@@ -1,3 +1,10 @@
 defmodule PentoWeb.Admin.SurveyResultsLive do
   use PentoWeb, :live_component
+
+  def update(assigns, socket) do
+    {:ok,
+     socket
+     |> assign(assigns)
+     |> assign_products_with_average_ratings()}
+  end
 end
