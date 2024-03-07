9a2c6d58bc18c1e9263a20bccfe78ad2f5379ac4
MunrraMT <dev.armsantos@gmail.com>
2024-03-07 00:36:30 -0400

    Render SVG Charts with Contex - Initialize the Dataset

--- cap8/pento/lib/pento_web/live/admin/survey_results_live.ex
+++ cap8/pento/lib/pento_web/live/admin/survey_results_live.ex
@@ -7,11 +7,25 @@ defmodule PentoWeb.Admin.SurveyResultsLive do
     {:ok,
      socket
      |> assign(assigns)
-     |> assign_products_with_average_ratings()}
+     |> assign_products_with_average_ratings()
+     |> assign_dataset()}
+  end
+
+  def assign_dataset(%{products_with_average_ratings: products_with_average_ratings} = socket) do
+    socket
+    |> assign(
+      :dataset,
+      make_bar_chart_dataset(products_with_average_ratings)
+    )
   end
 
   defp assign_products_with_average_ratings(socket) do
     socket
     |> assign(:products_with_average_ratings, Catalog.products_with_average_ratings())
+  end
+
+  defp make_bar_chart_dataset(data) do
+    data
+    |> Contex.Dataset.new()
   end
 end
