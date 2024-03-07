0a2ec29009e086e881972ad3304985b95c65e056
MunrraMT <dev.armsantos@gmail.com>
2024-03-07 00:50:46 -0400

    Render SVG Charts with Contex - Transform the Chart to SVG

--- cap8/pento/lib/pento_web/live/admin/survey_results_live.ex
+++ cap8/pento/lib/pento_web/live/admin/survey_results_live.ex
@@ -9,7 +9,8 @@ defmodule PentoWeb.Admin.SurveyResultsLive do
      |> assign(assigns)
      |> assign_products_with_average_ratings()
      |> assign_dataset()
-     |> assign_chart()}
+     |> assign_chart()
+     |> assign_chart_svg()}
   end
 
   def assign_dataset(%{products_with_average_ratings: products_with_average_ratings} = socket) do
@@ -18,6 +19,11 @@ defmodule PentoWeb.Admin.SurveyResultsLive do
       :dataset,
       make_bar_chart_dataset(products_with_average_ratings)
     )
+  end
+
+  def assign_chart_svg(%{assigns: %{chart: chart}} = socket) do
+    socket
+    |> assign(:chart_svg, render_bar_chart(chart))
   end
 
   defp assign_products_with_average_ratings(socket) do
@@ -39,4 +45,16 @@ defmodule PentoWeb.Admin.SurveyResultsLive do
     dataset
     |> Contex.BarChart.new()
   end
+
+  defp render_bar_chart(chart) do
+    Contex.Plot.new(500, 400, chart)
+    |> Contex.Plot.titles(title(), subtitle())
+    |> Contex.Plot.axis_labels(x_axis(), y_axis())
+    |> Contex.Plot.to_svg()
+  end
+
+  defp title(), do: "Product Ratings"
+  defp subtitle(), do: "average star ratings per product"
+  defp x_axis(), do: "products"
+  defp y_axis(), do: "stars"
 end
