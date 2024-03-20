7c3edc36ca74bb78186de24582f85a077c10fa5c
MunrraMT <dev.armsantos@gmail.com>
2024-03-19 22:54:49 -0400

    Render Points with SVG - Build the Point Component

--- cap12/pento/lib/pento_web/live/game_live/component.ex
+++ cap12/pento/lib/pento_web/live/game_live/component.ex
@@ -6,4 +6,28 @@ defmodule PentoWeb.GameLive.Component do
   import PentoWeb.GameLive.Colors
 
   @width 10
+
+  attr :x, :integer, required: true
+  attr :y, :integer, required: true
+  attr :fill, :string
+  attr :name, :string
+  attr :"phx-click", :string
+  attr :"phx-value", :string
+  attr :"phx-target", :any
+
+  def point(assigns) do
+    ~H"""
+    <use
+      xlink:href="#point"
+      x={convert(@x)}
+      y={convert(@y)}
+      fill={@fill}
+      phx-click="pick"
+      phx-value-name={@name}
+      phx-target="#game"
+    />
+    """
+  end
+
+  defp convert(i), do: (i - 1) * @width + 2 * @width
 end
