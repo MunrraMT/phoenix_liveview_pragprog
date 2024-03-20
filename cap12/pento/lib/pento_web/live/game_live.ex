0788e5301ee5bf627d665192efed0b563f3bdcc0
MunrraMT <dev.armsantos@gmail.com>
2024-03-19 22:45:22 -0400

    Render Points with SVG - Dynamically Draw Points with SVG <defs>

--- cap12/pento/lib/pento_web/live/game_live.ex
+++ cap12/pento/lib/pento_web/live/game_live.ex
@@ -9,7 +9,11 @@ defmodule PentoWeb.GameLive do
       <h1 class="font-bold text-3xl">Welcome to Pento!</h1>
     </section>
     <svg viewBox="0 0 100 100">
-      <rect x="0" y="0" width="10" height="10" />
+      <defs><rect id="point" width="10" height="10" /></defs>
+      <use xlink:href="#point" x="0" y="0" fill="blue" />
+      <use xlink:href="#point" x="10" y="0" fill="green" />
+      <use xlink:href="#point" x="0" y="10" fill="red" />
+      <use xlink:href="#point" x="10" y="10" fill="black" />
     </svg>
     """
   end
