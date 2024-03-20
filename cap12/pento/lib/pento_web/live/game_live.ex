46890d2500e4256ea63aa7357f83e167419c5da1
MunrraMT <dev.armsantos@gmail.com>
2024-03-19 23:48:38 -0400

    Put It All Together - Render the Board Component

--- cap12/pento/lib/pento_web/live/game_live.ex
+++ cap12/pento/lib/pento_web/live/game_live.ex
@@ -1,15 +1,21 @@
 defmodule PentoWeb.GameLive do
   use PentoWeb, :live_view
 
+  alias Pento.GameLive.Board
+
   import PentoWeb.GameLive.Component
 
-  def mount(_params, _session, socket), do: {:ok, socket}
+  def mount(%{"puzzle" => puzzle} = _params, _session, socket) do
+    {:ok,
+     socket
+     |> assign(:puzzle, puzzle)}
+  end
 
   def render(assigns) do
     ~H"""
     <section class="container">
       <h1 class="font-bold text-3xl">Welcome to Pento!</h1>
-      <.palette shape_names={[:i, :l, :y, :n, :p, :w, :u, :v, :s, :f, :x, :t]} />
+      <.live_component module={Board} puzzle={@puzzle} id="game" />
     </section>
     """
   end
